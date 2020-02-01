880faf6313e24ce2ae23743f85324a8d24df3477
Elizabeth Baumel <ebaumel@unity3d.com>
2020-02-01 17:20:14 -0500

    Add Monobehaviour character controller, comments for what coral growth system will do.

---
+++ ReeferMadness/Assets/Scripts/CoralGrowthSystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class CoralGrowthSystem : JobComponentSystem
+{
+    // Brain coral uniformly scales up until it gets to a maximum size, then it spawns a new one
+    [BurstCompile]
+    struct BrainCoralGrowthJob : IJobForEach<Translation, Rotation>
+    {
+        public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation)
+        {
+        }
+    }
+
+    // Shelf coral scales along 2 axes
+    // hard mode: make the boys wavy
+
+    // Branching coral scales up branches and then makes new segments
+    // easy mode: just make some new segments
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        return inputDependencies;
+    }
+}
